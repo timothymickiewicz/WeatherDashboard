e47a100b57b447522dd7dd790d382fe378d80e25
Timothy Mickiewicz <timothy.mickiewicz@gmail.com>
2020-03-13 04:33:07 -0500

    nested ajax call for uv index

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
 var btn = $(".btn");
 var citiesArray = [];
 var date = moment().format('MMMM Do YYYY, h:mm a');
+var cityDate = moment().format('MMMM Do YYYY');
 
 $("#date").text(date);
 // Making a refresh page function for when user submits their new city
@@ -25,19 +26,61 @@ btn.on("click", function() {
     console.log(citiesArray);
     refreshPage();
 });
+
 for (i=1; i < citiesArray.length; i++) {
   $("<button>").text(citiesArray[i]).addClass("cities").appendTo("#addedCities");
   $("</br>").appendTo("#addedCities"); 
   var APIKey = "7460ca674a63cbae5e3acbd439f037f5";
-  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + citiesArray[i] + "&appid=" + APIKey;
-    $.ajax({
+  var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + citiesArray[i] + "&appid=" + APIKey;
+      $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function(response) {
       console.log(response);
-      var city = response.name;
-      console.log(city);
-
+      var city = response.city.name;
+      var temp = response.list[0].main.temp;
+      temp = (temp - 273.15) * 9/5 + 32
+      temp = temp.toFixed(2)
+      var humidity = response.list[0].main.humidity;
+      var windSpeed = response.list[0].wind.speed;
+      var latitude = response.city.coord.lat;
+      var longitude = response.city.coord.lon;
+      var uvIndex = 0;
+      // Doing a second ajax query to obtain the UV index;
+      var queryURL2 = "http://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey + "&lat=" + latitude + "&lon=" + longitude + "";
+        $.ajax({
+          url: queryURL2,
+          method: "GET"
+        }).then(function(response2) {
+          console.log(response2);
+          uvIndex = response2.value;
+          var uvSpan = $("<span>").text(uvIndex).addClass("uvSpan");
+          // if (uvIndex > 10.00) {
+          //   uvSpan.css("background-color", "red");
+          // }
+          // else if (uvIndex > 5.00) {
+          //   uvSpan.css("background-color", "yellow");
+          // }
+          // else {
+          //   uvSpan.css("background-color", "blue");
+          // }
+          // $(".form-control:eq(" + i + ")").css("background-color", "teal")
+          $("<div>").appendTo("#wrapDash").addClass("uvInfo").text("UV Index: ").append(uvSpan);
+          if (uvIndex > 10.00) {
+            $("div").find("span").css("background-color", "red");
+          }
+          else if (uvIndex > 5.00) {
+            $("div").find("span").css("background-color", "yellow");
+          }
+          else if (uvIndex > 2.00) {
+            $("div").find("span").css("background-color", "lightblue");
+          }
+          // $( "p" ).find( "span" ).css( "color", "red" );
+        });
+      $("<div>").appendTo("#wrapDash").addClass("city").text(city + " (" + cityDate + ")");
+      $("<div>").appendTo("#wrapDash").addClass("cityInfo").text("Temperature: " + temp + " ℉");
+      $("<div>").appendTo("#wrapDash").addClass("cityInfo").text("Humidity: " + humidity + "%");
+      $("<div>").appendTo("#wrapDash").addClass("cityInfo").text("Wind Speed: " + windSpeed + " MPH");
     });
     
 }

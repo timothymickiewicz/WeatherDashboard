4cf48a81db99ddd4f11923eebdfe58b0ce927d48
Timothy Mickiewicz <timothy.mickiewicz@gmail.com>
2020-03-16 00:16:32 -0500

    cleaned up code, removed extras

--- assets/script.js
+++ assets/script.js
@@ -5,8 +5,15 @@ var cityDate = moment().format('MMMM Do YYYY');
 var APIKey = "7460ca674a63cbae5e3acbd439f037f5";
 var fiveDayTemp = 0;
 var fiveDayDate = "";
+var uvIndex = 0;
+var city = "";
+var temp = 0;
+var humidity = "";
+var windSpeed = "";
+var latitude = "";
+var longitude = "";
+// Puts date and time onto header
 $("#date").text(date);
-
 // Establishing default key values
 if (localStorage.getItem("cities") == null) {
   var defaultKey = "Nashville";
@@ -16,7 +23,6 @@ if (localStorage.getItem("cities") == null) {
 else {
   citiesArray = JSON.parse(localStorage.getItem("cities"));
 }
-
 // Adds a new city to the buttons array, sets the array, and refreshes page
 btn.on("click", function() {
   input = $(":text:eq(" + 0 + ")").val();
@@ -24,7 +30,6 @@ btn.on("click", function() {
   localStorage.setItem("cities", JSON.stringify(citiesArray));
   refreshPage();
 });
-
 // Grabbing the last entry in the citiesArray to load onto page
 function pageLoad() {
   loadBtns();
@@ -34,15 +39,14 @@ function pageLoad() {
     method: "GET"
   }).then(function(response) {
     console.log(response);
-    var city = response.city.name;
-    var temp = response.list[0].main.temp;
+    city = response.city.name;
+    temp = response.list[0].main.temp;
     temp = (temp - 273.15) * 9/5 + 32
     temp = temp.toFixed(2)
-    var humidity = response.list[0].main.humidity;
-    var windSpeed = response.list[0].wind.speed;
-    var latitude = response.city.coord.lat;
-    var longitude = response.city.coord.lon;
-    var uvIndex = 0;
+    humidity = response.list[0].main.humidity;
+    windSpeed = response.list[0].wind.speed;
+    latitude = response.city.coord.lat;
+    longitude = response.city.coord.lon;
     // Doing a second ajax query to obtain the UV index;
     var queryURL2 = "http://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey + "&lat=" + latitude + "&lon=" + longitude + "";
       $.ajax({
@@ -53,22 +57,11 @@ function pageLoad() {
         uvIndex = response2.value;
         var uvSpan = $("<span>").text(uvIndex).addClass("uvSpan");
         $("<div>").appendTo("#wrapDash").addClass("uvInfo").text("UV Index: ").append(uvSpan);
-        if (uvIndex > 10.00) {
-          $("div").find("span").css("background-color", "red");
-        }
-        else if (uvIndex > 5.00) {
-          $("div").find("span").css("background-color", "yellow");
-        }
-        else if (uvIndex > 2.00) {
-          $("div").find("span").css("background-color", "lightblue");
-        }
+        uvColor();
       });
-    $("<div>").appendTo("#wrapDash").addClass("city").text(city + " (" + cityDate + ")");
-    $("<div>").appendTo("#wrapDash").addClass("cityInfo").text("Temperature: " + temp + " ℉");
-    $("<div>").appendTo("#wrapDash").addClass("cityInfo").text("Humidity: " + humidity + "%");
-    $("<div>").appendTo("#wrapDash").addClass("cityInfo").text("Wind Speed: " + windSpeed + " MPH");
+      appendInformation();
     // Creating the 5 cards for the 5 day forecast
-    for (var j=0; j < 5; j++){
+    for (var j=0; j < 5; j++) {
       fiveDayDate = moment().add(j + 1, "day").format("MMMM Do YYYY");
       fiveDayTemp = response.list[j + 1].main.temp;
       fiveDayTemp = (fiveDayTemp - 273.15) * 9/5 + 32
@@ -84,12 +77,10 @@ function pageLoad() {
   });
 }
 pageLoad();
-
 // Making a refresh page function for when user submits their new city
 function refreshPage(){
     window.location.reload();
 }; 
-
 // Creating buttons on page load for local storage entries, and assigning click events for each to pull information on the selected city
 function loadBtns() {
   for (var i=1; i < citiesArray.length; i++) {
@@ -105,15 +96,14 @@ function loadBtns() {
       method: "GET"
     }).then(function(response) {
       console.log(response);
-      var city = response.city.name;
-      var temp = response.list[0].main.temp;
+      city = response.city.name;
+      temp = response.list[0].main.temp;
       temp = (temp - 273.15) * 9/5 + 32
       temp = temp.toFixed(2)
-      var humidity = response.list[0].main.humidity;
-      var windSpeed = response.list[0].wind.speed;
-      var latitude = response.city.coord.lat;
-      var longitude = response.city.coord.lon;
-      var uvIndex = 0;
+      humidity = response.list[0].main.humidity;
+      windSpeed = response.list[0].wind.speed;
+      latitude = response.city.coord.lat;
+      longitude = response.city.coord.lon;
       // Creating 5 day forecast
       for (var j=0; j < 5; j++){
         fiveDayDate = moment().add(j + 1, "day").format("MMMM Do YYYY");
@@ -137,25 +127,39 @@ function loadBtns() {
           uvIndex = response2.value;
           var uvSpan = $("<span>").text(uvIndex).addClass("uvSpan");
           $("<div>").appendTo("#wrapDash").addClass("uvInfo").text("UV Index: ").append(uvSpan);
-          if (uvIndex > 10.00) {
-            $("div").find("span").css("background-color", "red");
-          }
-          else if (uvIndex > 5.00) {
-            $("div").find("span").css("background-color", "yellow");
-          }
-          else if (uvIndex > 2.00) {
-            $("div").find("span").css("background-color", "lightblue");
-          }
+          uvColor();
         });
-      $("<div>").appendTo("#wrapDash").addClass("city").text(city + " (" + cityDate + ")");
-      $("<div>").appendTo("#wrapDash").addClass("cityInfo").text("Temperature: " + temp + " ℉");
-      $("<div>").appendTo("#wrapDash").addClass("cityInfo").text("Humidity: " + humidity + "%");
-      $("<div>").appendTo("#wrapDash").addClass("cityInfo").text("Wind Speed: " + windSpeed + " MPH");
+        appendInformation();
     });
     });  
   };
 };
-// When they click on a button, it will empty the main dash and 5 day display to be replaced with new information
+// Appends the main dashboard information to the page
+function appendInformation() {
+  $("<div>").appendTo("#wrapDash").addClass("city").text(city + " (" + cityDate + ")");
+  $("<div>").appendTo("#wrapDash").addClass("cityInfo").text("Temperature: " + temp + " ℉");
+  $("<div>").appendTo("#wrapDash").addClass("cityInfo").text("Humidity: " + humidity + "%");
+  $("<div>").appendTo("#wrapDash").addClass("cityInfo").text("Wind Speed: " + windSpeed + " MPH");
+}
+// Assigning the uv background color based on value
+function uvColor() {
+  if (uvIndex >= 11.00) {
+    $("div").find("span").css("background-color", "violet");
+  }
+  else if (uvIndex >= 8.00 && uvIndex < 11.00) {
+    $("div").find("span").css("background-color", "red");
+  }
+  else if (uvIndex >= 6.00 && uvIndex < 8.00) {
+    $("div").find("span").css("background-color", "orange");
+  }
+  else if (uvIndex >= 3.00 && uvIndex < 6.00) {
+    $("div").find("span").css("background-color", "yellow");
+  }
+  else if (uvIndex < 3.00) {
+    $("div").find("span").css("background-color", "lightblue");
+  }
+}
+// When they click on a button, it will empty the main dashboard and 5 day display and replace with the new information
 $(".cities").on("click", function() {
   $(".empty").empty();
 });

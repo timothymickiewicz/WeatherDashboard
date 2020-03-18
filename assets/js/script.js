93ed83800f8a02e0ad152e7badf0db39b4b42f2c
Timothy Mickiewicz <timothy.mickiewicz@gmail.com>
2020-03-18 04:29:46 -0500

    no more page refresh on user entry, turned forecast into function for shorter code

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,8 @@ var humidity = "";
 var windSpeed = "";
 var latitude = "";
 var longitude = "";
+var weatherIcon = "";
+var fiveDayHumidity = "";
 
 // Puts date and time onto header
 $("#date").text(date);
@@ -26,15 +28,56 @@ else {
   citiesArray = JSON.parse(localStorage.getItem("cities"));
 }
 
-// Adds a new city to the buttons array, sets the array, and refreshes page
+// Adds a new city to the buttons array, sets the array, clears dashboard and populates with the city they last entered
 btn.on("click", function() {
+  $(".empty").empty();
   input = $(":text:eq(" + 0 + ")").val().trim();
   citiesArray.push(input);
   localStorage.setItem("cities", JSON.stringify(citiesArray));
-  refreshPage();
-});
+  $(":text:eq(" + 0 + ")").val("")
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + input + "&appid=" + APIKey;
+      $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+      city = response.city.name;
+      temp = response.list[0].main.temp;
+      temp = (temp - 273.15) * 9/5 + 32
+      temp = temp.toFixed(2)
+      humidity = response.list[0].main.humidity;
+      windSpeed = response.list[0].wind.speed;
+      latitude = response.city.coord.lat;
+      longitude = response.city.coord.lon;
+      // Doing a second ajax query to obtain the UV index;
+      var queryURL2 = "http://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey + "&lat=" + latitude + "&lon=" + longitude + "";
+        $.ajax({
+          url: queryURL2,
+          method: "GET"
+        }).then(function(response2) {
+          uvIndex = response2.value;
+          var uvSpan = $("<span>").text(uvIndex).addClass("uvSpan");
+          $("<div>").appendTo("#wrapDash").addClass("uvInfo").text("UV Index: ").append(uvSpan);
+          uvColor();
+        });
+        for (j=0; j < 5; j++) {
+          fiveDayDate = moment().add(j + 1, "day").format("MMMM Do YYYY");
+          fiveDayTemp = response.list[j + 1].main.temp;
+          fiveDayTemp = (fiveDayTemp - 273.15) * 9/5 + 32;
+          fiveDayTemp = fiveDayTemp.toFixed(2);
+          weatherIcon = response.list[j + 1].weather[0].icon;
+          fiveDayHumidity = response.list[j + 1].main.humidity; 
+          appendForecast();
+        }
+        appendInformation();
+        $(".cities").remove();
+        $(".cities").addClass("hidden");
+        $(".cities").removeClass("hidden");
+        $("br").remove();
+        loadBtns();
+    });
+  });
 
-// Grabbing the last entry in the citiesArray to load onto page
+// Grabbing the last entry in the citiesArray to load onto page, gets called on page load
 function pageLoad() {
   loadBtns();
   var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + citiesArray[citiesArray.length - 1] + "&appid=" + APIKey;
@@ -61,80 +104,66 @@ function pageLoad() {
         $("<div>").appendTo("#wrapDash").addClass("uvInfo").text("UV Index: ").append(uvSpan);
         uvColor();
       });
-      appendInformation();
-    // Creating the 5 cards for the 5 day forecast
-    for (var j=0; j < 5; j++) {
-      fiveDayDate = moment().add(j + 1, "day").format("MMMM Do YYYY");
-      fiveDayTemp = response.list[j + 1].main.temp;
-      fiveDayTemp = (fiveDayTemp - 273.15) * 9/5 + 32
-      fiveDayTemp = fiveDayTemp.toFixed(2)
-      $("<div>").appendTo("#fiveDayCards").addClass("card");
-      $("<div>").appendTo(".card:eq(" + j + ")").addClass("card-body");
-      $("<h5>").appendTo(".card-body:eq(" + j + ")").addClass("card-title").text(fiveDayDate);
-      $("<div>").appendTo(".card-body:eq(" + j + ")").append("<img class='image' src=''/>");
-      $(".image:eq(" + j + ")").attr("src", "http://openweathermap.org/img/wn/" + response.list[j + 1].weather[0].icon + "@2x.png");
-      $("<p>").appendTo(".card-body:eq(" + j + ")").addClass("card-text").text("Temperature: " + fiveDayTemp + " ℉");
-      $("<p>").appendTo(".card-body:eq(" + j + ")").addClass("card-text").text("Humidity: " + response.list[j + 1].main.humidity + "%");
-    };
-  });
-}
-pageLoad();
-
-// Making a refresh page function for when user submits their new city
-function refreshPage(){
-    window.location.reload();
-}; 
-
-// Creating buttons on page load for local storage entries, and assigning click events for each to pull information on the selected city
-function loadBtns() {
-  for (var i=1; i < citiesArray.length; i++) {
-    // Creating new buttons with a text value of each index of citiesArray, with that same id value, appending to addedCities in html
-    $("<button>").text(citiesArray[i]).attr('id', citiesArray[i]).addClass("cities").appendTo("#addedCities");
-    $("</br>").appendTo("#addedCities");  
-    // Creating click events for the stored cities
-    $("#" + citiesArray[i]).on("click", function() {
-    var selectedCity = $(this).text();
-    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + selectedCity + "&appid=" + APIKey;
-      $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
-      city = response.city.name;
-      temp = response.list[0].main.temp;
-      temp = (temp - 273.15) * 9/5 + 32
-      temp = temp.toFixed(2)
-      humidity = response.list[0].main.humidity;
-      windSpeed = response.list[0].wind.speed;
-      latitude = response.city.coord.lat;
-      longitude = response.city.coord.lon;
-      // Creating 5 day forecast
-      for (var j=0; j < 5; j++){
+      for (j=0; j < 5; j++) {
         fiveDayDate = moment().add(j + 1, "day").format("MMMM Do YYYY");
         fiveDayTemp = response.list[j + 1].main.temp;
         fiveDayTemp = (fiveDayTemp - 273.15) * 9/5 + 32;
         fiveDayTemp = fiveDayTemp.toFixed(2);
-        $("<div>").appendTo("#fiveDayCards").addClass("card");
-        $("<div>").appendTo(".card:eq(" + j + ")").addClass("card-body");
-        $("<h5>").appendTo(".card-body:eq(" + j + ")").addClass("card-title").text(fiveDayDate);
-        $("<div>").appendTo(".card-body:eq(" + j + ")").append("<img class='image' src=''/>");
-        $(".image:eq(" + j + ")").attr("src", "http://openweathermap.org/img/wn/" + response.list[j + 1].weather[0].icon + "@2x.png");
-        $("<p>").appendTo(".card-body:eq(" + j + ")").addClass("card-text").text("Temperature: " + fiveDayTemp + " ℉");
-        $("<p>").appendTo(".card-body:eq(" + j + ")").addClass("card-text").text("Humidity: " + response.list[j + 1].main.humidity + "%");
-      };
-      // Doing a second ajax query to obtain the UV index;
-      var queryURL2 = "http://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey + "&lat=" + latitude + "&lon=" + longitude + "";
+        weatherIcon = response.list[j + 1].weather[0].icon;
+        fiveDayHumidity = response.list[j + 1].main.humidity;
+        appendForecast();
+      } 
+      appendInformation();
+  });
+};
+
+pageLoad();
+
+// Creating buttons for local storage entries, and assigning click events for each to pull information on the selected city
+function loadBtns() {
+  for (var i=1; i < citiesArray.length; i++) {
+      $("<button>").text(citiesArray[i]).attr('id', citiesArray[i]).addClass("cities").appendTo("#addedCities");
+      $("</br>").appendTo("#addedCities");  
+      // Creating click events for the stored cities
+      $("#" + citiesArray[i]).on("click", function() {
+        $(".empty").empty();
+        var selectedCity = $(this).text();
+        var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + selectedCity + "&appid=" + APIKey;
         $.ajax({
-          url: queryURL2,
+          url: queryURL,
           method: "GET"
-        }).then(function(response2) {
-          uvIndex = response2.value;
-          var uvSpan = $("<span>").text(uvIndex).addClass("uvSpan");
-          $("<div>").appendTo("#wrapDash").addClass("uvInfo").text("UV Index: ").append(uvSpan);
-          uvColor();
+        }).then(function(response) {
+          city = response.city.name;
+          temp = response.list[0].main.temp;
+          temp = (temp - 273.15) * 9/5 + 32
+          temp = temp.toFixed(2)
+          humidity = response.list[0].main.humidity;
+          windSpeed = response.list[0].wind.speed;
+          latitude = response.city.coord.lat;
+          longitude = response.city.coord.lon;
+          // Doing a second ajax query to obtain the UV index;
+          var queryURL2 = "http://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey + "&lat=" + latitude + "&lon=" + longitude + "";
+          $.ajax({
+            url: queryURL2,
+            method: "GET"
+          }).then(function(response2) {
+            uvIndex = response2.value;
+            var uvSpan = $("<span>").text(uvIndex).addClass("uvSpan");
+            $("<div>").appendTo("#wrapDash").addClass("uvInfo").text("UV Index: ").append(uvSpan);
+            uvColor();
+          });
+          for (j=0; j < 5; j++) {
+            fiveDayDate = moment().add(j + 1, "day").format("MMMM Do YYYY");
+            fiveDayTemp = response.list[j + 1].main.temp;
+            fiveDayTemp = (fiveDayTemp - 273.15) * 9/5 + 32;
+            fiveDayTemp = fiveDayTemp.toFixed(2);
+            weatherIcon = response.list[j + 1].weather[0].icon;
+            fiveDayHumidity = response.list[j + 1].main.humidity; 
+            appendForecast();
+          };
+          appendInformation();
         });
-        appendInformation();
-    });
-    });  
+      });  
   };
 };
 
@@ -145,6 +174,17 @@ function appendInformation() {
   $("<div>").appendTo("#wrapDash").addClass("cityInfo").text("Humidity: " + humidity + "%");
   $("<div>").appendTo("#wrapDash").addClass("cityInfo").text("Wind Speed: " + windSpeed + " MPH");
 }
+
+// Appends the forecast data into cards
+function appendForecast() {
+    $("<div>").appendTo("#fiveDayCards").addClass("card");
+    $("<div>").appendTo(".card:eq(" + j + ")").addClass("card-body");
+    $("<h5>").appendTo(".card-body:eq(" + j + ")").addClass("card-title").text(fiveDayDate);
+    $("<div>").appendTo(".card-body:eq(" + j + ")").append("<img class='image' src=''/>");
+    $(".image:eq(" + j + ")").attr("src", "http://openweathermap.org/img/wn/" + weatherIcon + "@2x.png");
+    $("<p>").appendTo(".card-body:eq(" + j + ")").addClass("card-text").text("Temperature: " + fiveDayTemp + " ℉");
+    $("<p>").appendTo(".card-body:eq(" + j + ")").addClass("card-text").text("Humidity: " + fiveDayHumidity + "%");
+};
 
 // Assigning the uv background color based on value
 function uvColor() {
@@ -164,8 +204,3 @@ function uvColor() {
     $("div").find("span").css("background-color", "lightblue");
   }
 }
-
-// When they click on a button, it will empty the main dashboard and 5 day display and replace with the new information
-$(".cities").on("click", function() {
-  $(".empty").empty();
-});
